4c560972f45dc5498e0b50ec1d4e503bcc9c1d9e
Tomasz Cielecki <tci@trackman.com>
2024-02-16 13:54:43 +0100

    Add sample

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using TrackMan.Api.Range.GqlV2;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection
+    .ConfigureClient(client =>
+    {
+        client.BaseAddress = new Uri("https://dr-cloud-api-dev.trackmangolfdev.com/graphql/");
+    });
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var client = serviceProvider.GetRequiredService<GraphQlClient>();
+var result = await client.ReportVirtualRange.ExecuteAsync("1234");
+// result.Data.Node.
---
+++ GqlClient/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GqlClient;
+
+public static class ConfigureGqlClient
+{
+    public static IServiceCollection ConfigureClient(
+        this IServiceCollection serviceCollection, Action<HttpClient> configureClient)
+    {
+        serviceCollection
+            .AddGraphQlClient()
+            .ConfigureHttpClient(configureClient);
+
+        return serviceCollection;
+    }
+}
